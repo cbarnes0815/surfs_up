aa4ae5838a131743d6999f261ce421fd581e1f9a
Courtney Barnes <courtneybarnes@Courtneys-Air.lan>
2022-07-26 03:55:00 -0400

    added routes

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ def stats(start=None, end=None):
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     if not end:
         results = session.query(*sel).\
-            filter(Measurement.date >= start).all()
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
         temps = list(np.ravel(results))
         return jsonify(temps=temps)
 
@@ -88,5 +89,7 @@ def stats(start=None, end=None):
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
-    return jsonify(temps)
+    return jsonify(temps=temps)
 
+if __name__ == "__main__":
+    app.run(debug=True)
